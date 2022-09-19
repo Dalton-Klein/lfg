5527f55d2121c320baf3b5cc88a922e297c69505
Dalton Klein <deklein@live.com>
2022-09-19 00:38:16 -0500

    updated image links to work with heroku

--- ui/src/components/tiles/daddyTile.tsx
+++ ui/src/components/tiles/daddyTile.tsx
@@ -1,16 +1,20 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./daddyTile.scss";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './daddyTile.scss';
 
 export default function DaddyTile(props: any) {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  let bgImage = "/assets/rust-tile-image.png";
-  return (
-    <article className="daddy-box" onClick={() => navigate(`${props.routerLink}`)} style={{ backgroundImage: `url(${bgImage})` }}>
-      <div className="daddy-gradient-overlay">
-        <h1 className="daddy-title">rust</h1>
-      </div>
-    </article>
-  );
+	let bgImage = '../assets/rust-tile-image.png';
+	return (
+		<article
+			className="daddy-box"
+			onClick={() => navigate(`${props.routerLink}`)}
+			style={{ backgroundImage: `url(${bgImage})` }}
+		>
+			<div className="daddy-gradient-overlay">
+				<h1 className="daddy-title">rust</h1>
+			</div>
+		</article>
+	);
 }
