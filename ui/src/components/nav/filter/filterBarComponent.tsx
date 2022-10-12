9ec4347847d45eaac57675c4c9e4dc07640c91c4
Dalton Klein <deklein@live.com>
2022-10-12 18:48:47 -0500

    finished clear all filters system

--- ui/src/components/nav/filter/filterBarComponent.tsx
+++ ui/src/components/nav/filter/filterBarComponent.tsx
@@ -10,10 +10,23 @@ import {
 	regionOptions,
 	sortOptions,
 } from '../../../utils/selectOptions';
+import { useRef } from 'react';
 
 export default function FilterBarComponent(props: any) {
+	const sortRef: any = useRef();
+	const hoursRef: any = useRef();
+	const ageRef: any = useRef();
+	const availabilityRef: any = useRef();
+	const languageRef: any = useRef();
+	const regionRef: any = useRef();
+
 	const clearFilters = () => {
 		props.clearFiltersMethod();
+		hoursRef.current!.clearFilter();
+		ageRef.current!.clearFilter();
+		availabilityRef.current!.clearFilter();
+		languageRef.current!.clearFilter();
+		regionRef.current!.clearFilter();
 	};
 
 	return (
@@ -21,12 +34,22 @@ export default function FilterBarComponent(props: any) {
 			<AccordionTab header="press for filters">
 				<div className="filter-bar">
 					<i className="pi pi-filter"></i>
-					<FilterComponent title="sort" options={sortOptions} multi={false}></FilterComponent>
-					<FilterComponent title="hours" options={hoursOptions} multi={true}></FilterComponent>
-					<FilterComponent title="age" options={ageOptions} multi={true}></FilterComponent>
-					<FilterComponent title="availability" options={availabilityOptions} multi={true}></FilterComponent>
-					<FilterComponent title="language" options={languageOptions} multi={true}></FilterComponent>
-					<FilterComponent title="region" options={regionOptions} multi={true}></FilterComponent>
+					<FilterComponent title="sort" options={sortOptions} multi={false} innerRef={sortRef}></FilterComponent>
+					<FilterComponent title="hours" options={hoursOptions} multi={true} innerRef={hoursRef}></FilterComponent>
+					<FilterComponent title="age" options={ageOptions} multi={true} innerRef={ageRef}></FilterComponent>
+					<FilterComponent
+						title="availability"
+						options={availabilityOptions}
+						multi={true}
+						innerRef={availabilityRef}
+					></FilterComponent>
+					<FilterComponent
+						title="language"
+						options={languageOptions}
+						multi={true}
+						innerRef={languageRef}
+					></FilterComponent>
+					<FilterComponent title="region" options={regionOptions} multi={true} innerRef={regionRef}></FilterComponent>
 					<div className="clear-button" onClick={clearFilters}>
 						clear all
 					</div>
