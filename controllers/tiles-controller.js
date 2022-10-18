2405852dba9b852d2f25017a4ed746ec89e92a53
Dalton Klein <deklein@live.com>
2022-10-18 15:17:20 -0500

    removed old code related to posts and categories

--- controllers/tiles-controller.js
+++ controllers/tiles-controller.js
@@ -51,45 +51,6 @@ const getRustTiles = async (req, res) => {
 	}
 };
 
-const createRustTile = async (req, res) => {
-	try {
-		const { owner, content, category, topics } = req.body.post;
-		let topicsColumnQueryString = '';
-		let topicsValueQueryString = '';
-		if (topics.length) {
-			topicsValueQueryString += `array [`;
-			topicsColumnQueryString = 'topics, ';
-			req.body.post.topics.forEach((topicId) => {
-				topicsValueQueryString += `${topicId}, `;
-			});
-			topicsValueQueryString = topicsValueQueryString.substring(0, topicsValueQueryString.length - 2);
-			topicsValueQueryString += '], ';
-		}
-		const reply = await await sequelize.query(
-			`
-      insert into posts (owner, content, categories, ${topicsColumnQueryString} number_votes, created_at, updated_at)
-      values (:owner, :content, :categories, ${topicsValueQueryString} :number_votes, now(), now())
-      `,
-			{
-				type: Sequelize.QueryTypes.INSERT,
-				replacements: {
-					owner,
-					content,
-					categories: category,
-					number_votes: 0,
-					created_at: `${Date.now()}`,
-					updated_at: `${Date.now()}`,
-				},
-			}
-		);
-		res.status(200).send(reply);
-	} catch (err) {
-		console.log(err);
-		res.status(500).send('POST ERROR');
-	}
-};
-
 module.exports = {
 	getRustTiles,
-	createRustTile,
 };
