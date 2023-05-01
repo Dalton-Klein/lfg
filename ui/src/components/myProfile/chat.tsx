da39401d8fc995afa01a24fd93892f9f3be32a1f
Dalton Klein <deklein@live.com>
2023-05-01 16:59:39 -0500

    made new nav system mobile friendly

--- ui/src/components/myProfile/chat.tsx
+++ ui/src/components/myProfile/chat.tsx
@@ -53,11 +53,6 @@ export default function Chat({ socketRef, convo }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  useEffect(() => {
-    console.log("setting current convo in chat?????", currentConvo);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentConvo]);
-
   //BEGIN Update messages list after each chat sent
   useEffect(() => {
     socketRef.current.on("message", ({ roomId, senderId, sender, message, timestamp }: any) => {
@@ -116,7 +111,6 @@ export default function Chat({ socketRef, convo }) {
 
   //BEGIN SOCKET Functions
   const updateCurrentConvo = () => {
-    console.log("currentConvo?????", currentConvo);
     if (convo.id === 0) {
       // @ts-ignore
       setcurrentConvo(JSON.parse(localStorage.getItem("currentConvo")));
