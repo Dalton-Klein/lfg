51b7cd34d37d8e6770b0b15d98981ac2c0036539
Dalton Klein <deklein@live.com>
2023-06-01 01:35:28 -0500

    test mobile play button???

--- ui/src/components/pages/gangPage.tsx
+++ ui/src/components/pages/gangPage.tsx
@@ -26,28 +26,33 @@ function isMobileDevice() {
 //   height: window.innerHeight / 2,
 //   width: window.innerWidth / 2,
 // };
+// const StyledAudio = styled.audio`
+//   height: 0%;
+//   width: 0%;
+// `;
+let StyledAudio: any;
+if (isMobileDevice()) {
+  // Some stuff for video can be used for screen sharing perhaps
+  StyledAudio = styled.audio`
+    height: 10%;
+    width: 10%;
+  `;
+} else {
+  StyledAudio = styled.audio`
+    height: 0%;
+    width: 0%;
+  `;
+}
 const Video = (props: any) => {
   const ref = useRef<any>();
   const [isPlaying, setIsPlaying] = useState(false);
-  let StyledAudio: any;
   useEffect(() => {
     if (ref.current && props.peer) {
       props.peer.on("stream", (stream: any) => {
         ref.current.srcObject = stream;
+        ref.current.play();
         setIsPlaying(true);
       });
-    }
-    if (isMobileDevice()) {
-      // Some stuff for video can be used for screen sharing perhaps
-      StyledAudio = styled.audio`
-        height: 10%;
-        width: 10%;
-      `;
-    } else {
-      StyledAudio = styled.audio`
-        height: 0%;
-        width: 0%;
-      `;
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
