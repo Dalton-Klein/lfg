139687ef602f5afb4239e7af2a8cf17b97c2b6a0
Dalton Klein <deklein@live.com>
2023-06-01 04:22:57 -0500

    electron implemented

---
+++ ui/public/main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+const url = require("url");
+require("dotenv").config();
+
+require("@electron/remote/main").initialize();
+
+// Create a new browser window
+function createWindow() {
+  let mainWindow = new BrowserWindow({
+    // Configure the browser window options
+    width: 800,
+    height: 600,
+    webPreferences: {
+      enableRemoteModule: true,
+      webSecurity: false,
+    },
+    icon: __dirname + "/build/assets/logoWhiteSmall.png",
+    autoHideMenuBar: true,
+  });
+
+  // Load your existing app's HTML file
+  if (isDev) {
+    mainWindow.loadURL("http://localhost:3000");
+  } else {
+    console.log("dirname?", __dirname);
+    mainWindow.loadURL(`file://${path.join(__dirname, "../build/index.html")}`);
+    mainWindow.webContents.openDevTools();
+  }
+
+  // Other code...
+
+  // Emitted when the window is closed
+  mainWindow.on("closed", () => {
+    // Dereference the window object
+    mainWindow = null;
+  });
+}
+
+// Run the createWindow function when Electron has finished initializing
+app.on("ready", createWindow);
