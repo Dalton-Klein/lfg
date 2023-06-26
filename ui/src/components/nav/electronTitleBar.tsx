3dca629d0af8e528813cb0c95289b557f7125a1f
Dalton Klein <deklein@live.com>
2023-06-26 14:07:18 -0500

    fix another build issue

--- ui/src/components/nav/electronTitleBar.tsx
+++ ui/src/components/nav/electronTitleBar.tsx
@@ -1,25 +1,26 @@
-import React, { useEffect, useState } from "react";
-import * as FS from "fs";
-import { IpcRenderer } from "electron";
-import "./electronTitleBar.scss";
-const fs: typeof FS = window.require("fs");
-const ipcRenderer: IpcRenderer = window.require("electron").ipcRenderer;
+// ***ELECTRON DISABLE ENTIRE COMPONENT
+// import React, { useEffect, useState } from "react";
+// import * as FS from "fs";
+// import { IpcRenderer } from "electron";
+// import "./electronTitleBar.scss";
+// const fs: typeof FS = window.require("fs");
+// const ipcRenderer: IpcRenderer = window.require("electron").ipcRenderer;
 
-const ElectronTitlebar = () => {
-  const [electronVersion, setElectronVersion] = useState("");
+// const ElectronTitlebar = () => {
+//   const [electronVersion, setElectronVersion] = useState("");
 
-  useEffect(() => {
-    ipcRenderer.invoke("getElectronVersion").then((version) => {
-      setElectronVersion(version);
-    });
-  }, []);
-  //restart the application :: again
-  return (
-    <div className="electron-titlebar">
-      {/* Titlebar content */}
-      <span>gangs v{electronVersion}</span>
-    </div>
-  );
-};
+//   useEffect(() => {
+//     ipcRenderer.invoke("getElectronVersion").then((version) => {
+//       setElectronVersion(version);
+//     });
+//   }, []);
+//   //restart the application :: again
+//   return (
+//     <div className="electron-titlebar">
+//       {/* Titlebar content */}
+//       <span>gangs v{electronVersion}</span>
+//     </div>
+//   );
+// };
 
-export default ElectronTitlebar;
+// export default ElectronTitlebar;
