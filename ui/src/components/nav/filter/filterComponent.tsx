0212a56e899a13090799c6cf4a858b29c5b88d0f
Dalton Klein <deklein@live.com>
2023-07-01 16:11:03 -0500

    fixed clear all filters feature

--- ui/src/components/nav/filter/filterComponent.tsx
+++ ui/src/components/nav/filter/filterComponent.tsx
@@ -1,119 +1,140 @@
-import './filterComponent.scss';
-import 'primeicons/primeicons.css';
-import makeAnimated from 'react-select/animated';
-import { useImperativeHandle, useState } from 'react';
-import Select from 'react-select';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../../store/store';
-import { setPreferences } from '../../../store/userPreferencesSlice';
+import "./filterComponent.scss";
+import "primeicons/primeicons.css";
+import makeAnimated from "react-select/animated";
+import { useImperativeHandle, useState } from "react";
+import Select from "react-select";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../store/store";
+import { setPreferences } from "../../../store/userPreferencesSlice";
 
 const animatedComponents = makeAnimated();
 interface props {
-	title: string;
-	options: any;
-	multi: boolean;
-	innerRef: any;
+  title: string;
+  options: any;
+  multi: boolean;
+  innerRef: any;
 }
 
 const style = {
-	control: (base: any) => ({
-		...base,
-		// This line disables the default blue border in react-select
-		boxShadow: 'none',
+  control: (base: any) => ({
+    ...base,
+    // This line disables the default blue border in react-select
+    boxShadow: "none",
   }),
   menu: (base: any) => ({
-		...base,
-		// This line disables the default blue border in react-select
-		backgroundColor: '#1c1c1e',
-		borderRadius: 'calc(8px + 0.25vw)',
+    ...base,
+    // This line disables the default blue border in react-select
+    backgroundColor: "#1c1c1e",
+    borderRadius: "calc(8px + 0.25vw)",
   }),
   menuList: (base: any) => ({
-		...base,
-		// This line disables the default blue border in react-select
-		backgroundColor: 'transparent',
+    ...base,
+    // This line disables the default blue border in react-select
+    backgroundColor: "transparent",
   }),
   option: (base: any) => ({
-		...base,
-    borderRadius: 'calc(8px + 0.25vw)',
-    backgroundColor: ' #1c1c1e',
-    color: '#ffffff',
-    fontSize: 'calc(16px + 0.25vw)',
+    ...base,
+    borderRadius: "calc(8px + 0.25vw)",
+    backgroundColor: " #1c1c1e",
+    color: "#ffffff",
+    fontSize: "calc(16px + 0.25vw)",
     opacity: 1,
     "&:hover": {
-      backgroundColor: '#232026',
+      backgroundColor: "#232026",
     },
   }),
 };
 
 export default function FilterComponent(props: props) {
-	const preferencesState = useSelector((state: RootState) => state.preferences);
-	const dispatch = useDispatch();
-	const [selected, setSelected] = useState<any>();
+  const preferencesState = useSelector((state: RootState) => state.preferences);
+  const dispatch = useDispatch();
+  const [selected, setSelected] = useState<any>();
 
-	useImperativeHandle(props.innerRef, () => ({
-		clearFilter() {
-			selectionChange([], {}, true);
-		},
-	}));
+  useImperativeHandle(props.innerRef, () => ({
+    clearFilter: clearFilter,
+  }));
 
-	const selectionChange = (options: any, filterAction: any, isClearing = false) => {
+  const clearFilter = () => {
+    selectionChange([], {}, true);
+  };
+
+  const selectionChange = (options: any, filterAction: any, isClearing = false) => {
     setSelected(options);
-    let formattedOptions:any = [];
+    let formattedOptions: any = [];
     //For options that are pictures, use this if block
-    if (filterAction.name === 'rank') {
-      options.forEach((option:any) => {
-        formattedOptions.push(option.label.props.alt)
+    if (filterAction.name === "rank") {
+      options.forEach((option: any) => {
+        formattedOptions.push(option.label.props.alt);
       });
     }
     //Update preferences state with filter objects
-		if (!isClearing) {
-			const filterName = filterAction.name;
-			dispatch(
-				setPreferences({
-					...preferencesState,
-					discoverFilters: { ...preferencesState.discoverFilters, [filterName]: formattedOptions.length ? formattedOptions:options },
-				})
-			);
-		}
-	};
+    if (isClearing) {
+      dispatch(
+        setPreferences({
+          ...preferencesState,
+          discoverFilters: {
+            sort: "",
+            age: [],
+            hours: [],
+            availability: [],
+            language: [],
+            region: [],
+            playlist: [],
+            rank: [],
+          },
+        })
+      );
+    } else {
+      const filterName = filterAction.name;
+      dispatch(
+        setPreferences({
+          ...preferencesState,
+          discoverFilters: {
+            ...preferencesState.discoverFilters,
+            [filterName]: formattedOptions.length ? formattedOptions : options,
+          },
+        })
+      );
+    }
+  };
 
-	return (
-		<div className='filter-container'>
-			{props.multi ? (
-				<Select
-					name={props.title}
-					value={selected || ''}
-					components={animatedComponents}
-					options={props.options}
-					className='react-select-container'
-					classNamePrefix='react-select'
-					placeholder={props.title}
-					isClearable={false}
-					isSearchable={false}
-					styles={style}
-					onChange={selectionChange}
+  return (
+    <div className="filter-container">
+      {props.multi ? (
+        <Select
+          name={props.title}
+          value={selected || ""}
+          components={animatedComponents}
+          options={props.options}
+          className="react-select-container"
+          classNamePrefix="react-select"
+          placeholder={props.title}
+          isClearable={false}
+          isSearchable={false}
+          styles={style}
+          onChange={selectionChange}
           isMulti
-					menuPortalTarget={document.body}
-					menuPosition={'fixed'}
-					menuShouldBlockScroll={true}
-				/>
-			) : (
-				<Select
-					name={props.title}
-					components={animatedComponents}
-					options={props.options}
-					className='react-select-container'
-					classNamePrefix='react-select'
-					placeholder={props.title}
-					isClearable={false}
-					isSearchable={false}
-					styles={style}
-					onChange={selectionChange}
-					menuPortalTarget={document.body}
-					menuPosition={'fixed'}
-					menuShouldBlockScroll={true}
-				/>
-			)}
-		</div>
-	);
+          menuPortalTarget={document.body}
+          menuPosition={"fixed"}
+          menuShouldBlockScroll={true}
+        />
+      ) : (
+        <Select
+          name={props.title}
+          components={animatedComponents}
+          options={props.options}
+          className="react-select-container"
+          classNamePrefix="react-select"
+          placeholder={props.title}
+          isClearable={false}
+          isSearchable={false}
+          styles={style}
+          onChange={selectionChange}
+          menuPortalTarget={document.body}
+          menuPosition={"fixed"}
+          menuShouldBlockScroll={true}
+        />
+      )}
+    </div>
+  );
 }
